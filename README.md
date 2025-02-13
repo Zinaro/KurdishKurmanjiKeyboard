cc7b5a75c5fe360c4e3c98b48da3e57cdaddccea
Zinaro <zinar@github.com>
2025-02-14 02:19:55 +0300

    installation file added

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# KurdishKurmanjiKeyboard
-kurdish keyboard for linux
+# Kurdish Kurmanji Keyboard
+Kurdish (Kurmanji) keyboard for linux
+
+## Installation
+
+1. Clone the repository:
+   ```bash
+   git clone https://github.com/Zinaro/KurdishKurmanjiKeyboard.git
+   cd KurdishKurmanjiKeyboard
+   ```
+
+2. Run the installation script:
+    ```
+    chmod +x install.sh
+    ./install.sh
+    ```
+3. Activate the keyboard layout:
+    ```
+    setxkbmap ku
+    ```
---
+++ install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+KEYMAP="ku"
+DESCRIPTION="Kurdish (Kurmanji)"
+SOURCE_FILE="${KEYMAP}"
+DEST_DIR="/usr/share/X11/xkb/symbols/"
+EVDEV_XML="/usr/share/X11/xkb/rules/evdev.xml"
+BASE_XML="/usr/share/X11/xkb/rules/base.xml"
+
+sudo cp $SOURCE_FILE $DEST_DIR
+
+DISTRO=$(grep ^ID= /etc/os-release | cut -d= -f2 | tr -d '"')
+
+if [[ "$DISTRO" == "ubuntu" || "$DISTRO" == "debian" ]]; then
+    XML_FILE=$EVDEV_XML
+elif [[ "$DISTRO" == "fedora" || "$DISTRO" == "centos" ]]; then
+    XML_FILE=$BASE_XML
+else
+    XML_FILE=$EVDEV_XML
+fi  
+
+LAYOUT_CHECK="\    <layout>\n      <configItem>\n        <name>$KEYMAP</name>\n        <shortDescription>ku</shortDescription>\n        <description>$DESCRIPTION</description>\n        <languageList>\n          <iso639Id>kur</iso639Id>\n        </languageList>\n      </configItem>\n    </layout>"
+
+if ! grep -Fxq "$LAYOUT_CHECK" "$XML_FILE"; then
+    sudo sed -i "/<\/layoutList>/i $LAYOUT_CHECK" "$XML_FILE"
+    echo "Layout tevlî XML bû."
+else
+    echo "Layout jixwe di XML de heye."
+fi
+
+
+setxkbmap $KEYMAP
+
+echo "Klavye bi serkeftî hat sazkirin. Distro: $DISTRO"
